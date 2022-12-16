f657baec6880680e01b15d97f82ba2379080dff5
dani430b <95855777+dani430b@users.noreply.github.com>
2022-12-16 22:22:51 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdzJUH7gDC505qJoD2XlUna6QnGSinPh4",
-  authDomain: "dragdropdb.firebaseapp.com",
-  projectId: "dragdropdb",
-  storageBucket: "dragdropdb.appspot.com",
-  messagingSenderId: "316157142104",
-  appId: "1:316157142104:web:d5eb6a2057cae1e2b1962d",
-  measurementId: "G-PT5RBWZXYP"
+  apiKey:
+  authDomain:
+  projectId: 
+  storageBucket: 
+  messagingSenderId:
+  appId: 
+  measurementId:
 };
 
 // Initialize Firebase
